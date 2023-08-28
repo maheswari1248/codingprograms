fa7231cd402d605ecfd9cb085a3e46a3f1ed3015
maheswari1248 <122718261+maheswari1248@users.noreply.github.com>
2023-08-28 17:53:43 +0530

    Update duplicatevalues.java

--- rotation/duplicatevalues.java
+++ rotation/duplicatevalues.java
@@ -8,7 +8,7 @@ public class duplicatevalues {
 		for(int i=0;i<a.length;i++) {
 		for(int j=i+1;j<a.length;j++) {
 			if(a[i]==a[j]) {
-				System.out.println(a[i]); 
+				System.out.println(a[i]+" "); 
 			}
 		
 			
